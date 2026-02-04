70b105d4b7e99395abbdc2f4c9f399c012b62a62
Aiganim <zhanadilova.aiganim@gmail.com>
2026-02-04 17:20:15 +0500

    Assignment 8: templates, deployment

--- library/views.py
+++ library/views.py
@@ -1,3 +1,5 @@
+# API view to retrieve all books
+
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Book, BorrowRecord
